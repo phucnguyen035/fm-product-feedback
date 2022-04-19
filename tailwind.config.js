3799e8873fecc42e1a8b68d1d2caa40f94c8f68b
Phuc Nguyen <40336551+phucnguyen035@users.noreply.github.com>
2022-04-19 13:52:27 +0700

    style(styles): move tailwind fontSize config to extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,15 +5,6 @@ module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
 
   theme: {
-    fontSize: {
-      h1: ['24px', { lineHeight: '35px', letterSpacing: '-0.33px' }],
-      h2: ['20px', { lineHeight: '29px', letterSpacing: '-0.25px' }],
-      h3: ['18px', { lineHeight: '26px', letterSpacing: '-0.25px' }],
-      h4: ['14px', { lineHeight: '20px', letterSpacing: '0.2px' }],
-      body1: ['16px', '23px'],
-      body2: ['15px', '22px'],
-      body3: ['13px', '19px'],
-    },
     colors: {
       pink: '#AD1FEA',
       blue: '#4661E6',
@@ -32,6 +23,15 @@ module.exports = {
       },
     },
     extend: {
+      fontSize: {
+        h1: ['24px', { lineHeight: '35px', letterSpacing: '-0.33px' }],
+        h2: ['20px', { lineHeight: '29px', letterSpacing: '-0.25px' }],
+        h3: ['18px', { lineHeight: '26px', letterSpacing: '-0.25px' }],
+        h4: ['14px', { lineHeight: '20px', letterSpacing: '0.2px' }],
+        body1: ['16px', '23px'],
+        body2: ['15px', '22px'],
+        body3: ['13px', '19px'],
+      },
       fontFamily: {
         sans: ['Jost', ...defaultTheme.fontFamily.sans],
       },
