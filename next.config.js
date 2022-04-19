fc4410c44f2ab57671eaa9217ada193776a5d8cf
Phuc Nguyen <40336551+phucnguyen035@users.noreply.github.com>
2022-04-19 19:40:57 +0700

    chore: disable react strict mode
    due to react 18, some api in headless ui is broken

--- next.config.js
+++ next.config.js
@@ -13,5 +13,6 @@ module.exports = withBundleAnalyzer({
   // The starter code load resources from `public` folder with `router.basePath` in React components.
   // So, the source code is "basePath-ready".
   // You can remove `basePath` if you don't need it.
-  reactStrictMode: true,
+  // TODO: Enable again once headlessui has updated
+  reactStrictMode: false,
 });
