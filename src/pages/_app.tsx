173ace2426e197050d0e5f0311b4e6227fd85b02
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2022-05-15 23:47:48 +0700

    feat: add layout and auth check in _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,33 @@
 import '../styles/global.css';
 
+import { NextPage } from 'next';
 import { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
+import { ReactElement, ReactNode, useCallback } from 'react';
 
-const MyApp = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => {
+import { ProtectedPage } from '@/components/ProtectedPage';
+
+type MyNextPage = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+  auth?: boolean;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: MyNextPage;
+};
+
+const MyApp = ({ Component, pageProps: { session, ...pageProps } }: AppPropsWithLayout) => {
+  const { auth } = Component;
+
+  const renderPage = useCallback(() => {
+    const getLayout = Component.getLayout ?? ((page) => page);
+
+    return getLayout(<Component {...pageProps} />);
+  }, []);
+
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      {auth ? <ProtectedPage>{renderPage()}</ProtectedPage> : renderPage()}
     </SessionProvider>
   );
 };
